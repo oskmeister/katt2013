908d49683635e36b88a83cf91e2c402e56c88dfb
Oskar Werkelin Ahlin <oswa@csc.kth.se>
2013-03-03 21:29:49 +0100

    orkester: added input validator

---
+++ orkester/input_format_validators/validate.py
@@ -0,0 +1,32 @@
+from sys import stdin
+import re
+import sys
+import math
+
+number_pat = "[1-9]\d* [1-9]\d*\n"
+int_re = re.compile(number_pat)
+
+zint_pat = "\d*"
+zint_re = re.compile(zint_pat)
+
+line = stdin.readline()
+
+# first line, two integers
+assert int_re.match(line)
+
+N,M = map(int,line.strip().split())
+assert 1 <= N <= 1000 
+assert 1 <= M <= 200 
+
+# check two lines with N numbers
+for i in xrange(N):
+    line = stdin.readline()
+    for s in line.strip().split():
+        assert zint_re.match(s)
+    line_splitted = map(int,line.strip().split())
+    assert line_splitted[0] == len(line_splitted)-1
+    for x in line_splitted[1:]:
+        assert 1 <= x <= M
+
+assert len(stdin.readline()) == 0
+sys.exit(42)
