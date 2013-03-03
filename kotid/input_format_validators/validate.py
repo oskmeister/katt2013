bb7b83473defeb0e92acad48f40ef5c66928e789
Oskar Werkelin Ahlin <oswa@csc.kth.se>
2013-03-03 15:28:04 +0100

    kotid: validator

---
+++ kotid/input_format_validators/validate.py
@@ -0,0 +1,26 @@
+from sys import stdin
+import re
+import sys
+import math
+
+number_pat = "[1-9]\d*"
+twonumbers_pat = number_pat + " " + number_pat + "\n"
+int2_re = re.compile(twonumbers_pat)
+
+line = stdin.readline()
+
+# first line, one integer.    
+assert int2_re.match(line)
+
+n,k = map(int,line.strip().split())
+assert 1 <= n <= 100000 
+assert 1 <= k <= n
+
+# check two lines with N numbers
+line = stdin.readline()
+assert len(line.strip().split()) == n
+for x in line.strip().split():
+    assert 1 <= int(x) <= k
+
+assert len(stdin.readline()) == 0
+sys.exit(42)
